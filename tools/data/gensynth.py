2975d36ec3001c3b12baaa5245fe4226dd28d8e4
Nguyen Manh Tan <tan@tanmaclocalhost.lan>
2024-09-20 06:03:45 -0400

    refactored synthetic data generation

--- tools/data/gensynth.py
+++ tools/data/gensynth.py
@@ -42,12 +42,15 @@ def _random_color(start: int, end: int, opacity: Optional[int]) -> ColorTuple:
     return red, green, blue, opacity
 
 
-class ModifiedImageCaptcha(ImageCaptcha):
+class EmptyBackgroundImageCaptcha(ImageCaptcha):
     # Override
     def generate_image(self, chars: str) -> Image:
-        """Generate the image of the given characters.
-
-        :param chars: text to be generated.
+        """
+        Generate the image of the given characters with an empty background set to 0.
+        Args:
+            chars (str): Characters to generate the image.
+        Returns:
+            Image: Captcha image of the given characters.
         """
         background = 0, 0, 0, 0
         color = _random_color(100, 250, 255)
@@ -58,199 +61,229 @@ class ModifiedImageCaptcha(ImageCaptcha):
         return im
 
 
-def generate_text_lines(text_tokens, max_tokens, max_lines):
-    """
-    Generate text lines. The function generates a random selection of tokens and splits them into lines.
-    Args:
-        text_tokens (list): List of text tokens.
-        max_tokens (int): Maximum number of tokens.
-        max_lines (int): Maximum number of lines of text
-    Returns:
-        list: List of text lines.
-    """
-    # Generate a random selection of tokens
-    text = np.random.choice(text_tokens, np.random.randint(1, max_tokens))
+class MultiCaptchaImageGenerator:
+    def __init__(
+        self,
+        height: int,
+        width: int,
+        text_tokens: List[str],
+        max_tokens: int,
+        max_lines: int,
+    ):
+        self.height = height
+        self.width = width
+        self.text_tokens = text_tokens
+        self.max_tokens = max_tokens
+        self.max_lines = max_lines
 
-    # Generate random split points without replacement and sort them
-    split_points = sorted(
-        np.random.choice(
-            range(1, len(text)),
-            np.random.randint(1, min(max_lines, len(text)) + 1) - 1,
-            replace=False,
+    def generate_captcha_texts(self):
+        """
+        Generate captcha text lines.
+        The function generates a random selection of tokens and splits them into lines.
+        Returns:
+            list: List of text lines.
+        """
+        # Generate a random selection of tokens
+        text = np.random.choice(self.text_tokens, np.random.randint(1, self.max_tokens))
+
+        # Generate random split points without replacement and sort them
+        split_points = sorted(
+            np.random.choice(
+                range(1, len(text)),
+                np.random.randint(1, min(self.max_lines, len(text)) + 1) - 1,
+                replace=False,
+            )
         )
-    )
 
-    # Split the text based on the split points
-    content = [
-        "".join(text[start:end])
-        for start, end in zip([0] + split_points, split_points + [len(text)])
-    ]
+        # Split the text based on the split points
+        text_lines = [
+            "".join(text[start:end])
+            for start, end in zip([0] + split_points, split_points + [len(text)])
+        ]
 
-    return content
+        return text_lines
+
+    def generate_captcha_attribute(self):
+        """
+        Generate captcha object attributes.
+        Return:
+            dict: Object attributes all in relative values:
+                - x: x-coordinate of the object center.
+                - y: y-coordinate of the object center.
+                - w: width of the object.
+                - h: height of the object.
+                - rotation: rotation of the object.
+                - content: content of the object. The content should be a list of string, where each element
+                    corresponds to a line of text.
+        """
+
+        w = np.random.uniform(0.3, 0.5)
+        h = np.random.uniform(0.3, 0.5)
+        x = np.random.uniform(w / 2, 1 - w / 2)
+        y = np.random.uniform(h / 2, 1 - h / 2)
+        rotation = np.random.uniform(-50, 50)
+        text_lines = self.generate_captcha_texts()
+
+        return {
+            "x": x,
+            "y": y,
+            "w": w,
+            "h": h,
+            "rotation": rotation,
+            "text_lines": text_lines,
+        }
+
+    def is_captcha_intersect(self, captcha_att_list: List[dict], captcha_att: dict):
+        """
+        Check if the captcha object intersects with any of the existing captcha objects before rotation.
+        Args:
+            captcha_att_list (list): List of captcha object attributes.
+            captcha_att (dict): Captcha object attributes.
+        Return:
+            bool: True if the captcha object intersects with any of the existing captcha objects.
+        """
+        x = captcha_att["x"]
+        y = captcha_att["y"]
+        w = captcha_att["w"]
+        h = captcha_att["h"]
+
+        for att in captcha_att_list:
+            x1 = att["x"]
+            y1 = att["y"]
+            w1 = att["w"]
+            h1 = att["h"]
+
+            if x < x1 + w1 and x + w > x1 and y < y1 + h1 and y + h > y1:
+                return True
+
+        return False
+
+    def insert_captcha_object(self, img: np.ndarray, captcha_att: dict):
+        """
+        Inplace insert a captcha object into the image.
+        The captcha attribute is used to rotate and insert the object into the image.
+        After the insertion, the attribute is updated correspondingly.
+        Args:
+            img (np.ndarray): Image to insert the object into.
+            captcha_att (dict): Captcha object attributes.
+        """
+        # Get the captcha object attributes
+        x = int(captcha_att["x"] * img.shape[1])
+        y = int(captcha_att["y"] * img.shape[0])
+        w = int(captcha_att["w"] * img.shape[1])
+        h = int(captcha_att["h"] * img.shape[0])
+        rotation = captcha_att["rotation"]
+        text_lines = captcha_att["text_lines"]
+
+        img_list = []
+        for text in text_lines:
+            imageCaptcha = EmptyBackgroundImageCaptcha(height=h, width=w)
+            text_img = imageCaptcha.generate_image(text)
+            img_list.append(text_img)
+
+        # stack the image list vertically
+        stacked_text = np.vstack(img_list)
+
+        # add a border line to the stacked text
+        stacked_text = cv2.copyMakeBorder(
+            stacked_text, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=(100, 100, 100)
+        )
+
+        # Rotate the image and expand it to fit the new size, padding with white
+        stacked_text = Image.fromarray(stacked_text)
+        stacked_text = np.array(
+            stacked_text.rotate(rotation, expand=True, fillcolor=(0, 0, 0))
+        )
+
+        # resize the image to the object size
+        stacked_text = cv2.resize(stacked_text, (w, h))
+
+        # Insert the rotated text into the image
+        img[
+            y - h // 2 : y - h // 2 + h,
+            x - w // 2 : x - w // 2 + w,
+        ] += stacked_text
+
+    def generate_multi_captcha_image(self, num_max_objs: int, add_noise: bool):
+        """
+        Generate an image with multiple captcha objects.
+        The number of captcha objects is upper bounded by num_max_objs.
+
+        Args:
+            num_max_objs (int): Maximum number of captcha objects to generate.
+            add_noise (bool): Whether to add noise to the image.
+        Returns:
+            np.ndarray: Image with multiple captcha objects.
+            list: List of captcha object attributes.
+        """
+        img = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+
+        # 1. Add multiple captcha objects into the image.
+        captcha_att_list = []
+        for _ in range(num_max_objs):
+            captcha_att = self.generate_captcha_attribute()
+
+            if not self.is_captcha_intersect(captcha_att_list, captcha_att):
+                self.insert_captcha_object(img, captcha_att)
+                captcha_att_list.append(captcha_att)
+
+        img = cv2.bitwise_not(img)
+
+        # 2. Add noise to the image.
+        if add_noise:
+            noise = np.random.randint(0, 255, img.shape, dtype=np.uint8)
+            img = cv2.addWeighted(img, 0.7, noise, 0.3, 0)
+
+        return img, captcha_att_list
 
 
-def insert_object(img, obj_attr):
-    """
-    Inplace insert an object into the image.
-    Args:
-        img (np.ndarray): Image to insert the object into.
-        obj_attr (dict): Object attributes.
-
-    """
-    # Get the object attributes
-    x = int(obj_attr["x"] * img.shape[1])
-    y = int(obj_attr["y"] * img.shape[0])
-    w = int(obj_attr["w"] * img.shape[1])
-    h = int(obj_attr["h"] * img.shape[0])
-    rotation = obj_attr["rotation"]
-    content = obj_attr["content"]
-
-    img_list = []
-    for text in content:
-        imageCaptcha = ModifiedImageCaptcha(height=h, width=w)
-        text_img = imageCaptcha.generate_image(text)
-        img_list.append(text_img)
-
-    # stack the image list vertically
-    stacked_text = np.vstack(img_list)
-
-    # add a border line to the stacked text
-    stacked_text = cv2.copyMakeBorder(
-        stacked_text, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=(100, 100, 100)
-    )
-
-    # Rotate the image and expand it to fit the new size, padding with white
-    stacked_text = Image.fromarray(stacked_text)
-    stacked_text = np.array(
-        stacked_text.rotate(rotation, expand=True, fillcolor=(0, 0, 0))
-    )
-
-    # resize the image to the object size
-    stacked_text = cv2.resize(stacked_text, (w, h))
-
-    # Insert the rotated text into the image
-    img[y : y + stacked_text.shape[0], x : x + stacked_text.shape[1]] += stacked_text
-
-
-def generate_object_attribute(text_tokens: List[str], max_tokens: int, max_lines: int):
-    """
-    Generate object attributes.
-
-    Args:
-        text_tokens (list): List of text tokens.
-        max_tokens (int): Maximum number of tokens.
-        max_lines (int): Maximum number of lines of text
-    Return:
-        dict: Object attributes all in relative values:
-            - x: x-coordinate of the object top left.
-            - y: y-coordinate of the object top left.
-            - w: width of the object.
-            - h: height of the object.
-            - rotation: rotation of the object.
-            - content: content of the object. The content should be a list of string, where each element
-                corresponds to a line of text.
-    """
-
-    w = np.random.uniform(0.3, 0.5)
-    h = np.random.uniform(0.3, 0.5)
-    x = np.random.uniform(0, 1 - w)
-    y = np.random.uniform(0, 1 - h)
-    rotation = np.random.uniform(0, 80)
-    content = generate_text_lines(text_tokens, max_tokens, max_lines)
-
-    return {
-        "x": x,
-        "y": y,
-        "w": w,
-        "h": h,
-        "rotation": rotation,
-        "content": content,
-    }
-
-
-def att_intersect(att_list, obj_attr):
-    """
-    Check if the object intersects with any of the existing objects.
-
-    Args:
-        att_list (list): List of object attributes.
-        obj_attr (dict): Object attributes.
-
-    Returns:
-            bool: True if the object intersects with any of the existing objects.
-    """
-    x = obj_attr["x"]
-    y = obj_attr["y"]
-    w = obj_attr["w"]
-    h = obj_attr["h"]
-
-    for att in att_list:
-        x1 = att["x"]
-        y1 = att["y"]
-        w1 = att["w"]
-        h1 = att["h"]
-
-        if x < x1 + w1 and x + w > x1 and y < y1 + h1 and y + h > y1:
-            return True
-
-    return False
-
-
-def gen_and_save_image(image_idx, text_tokens, max_tokens, height, width, result_dir):
+def gen_and_save_image(
+    image_idx: int,
+    num_max_objs: int,
+    multicaptcha_image_generator: MultiCaptchaImageGenerator,
+    result_dir: str,
+):
     """
     Generate and save an image and its labels.
     Args:
         image_idx (int): Index of the image.
-        text_tokens (list): List of text tokens.
-        max_tokens (int): Maximum number of tokens.
-        height (int): Height of the image.
-        width (int): Width of the image.
-        result_dir (str): Path to the directory to save the
-            synthetic images.
+        num_max_objs (int): Maximum number of captcha objects to generate.
+        multicaptcha_image_generator (MultiCaptchaImageGenerator): MultiCaptchaImageGenerator instance.
+        result_dir (str): Path to the directory to save the images.
     """
-    # 1. generate a random number of objects
-    att_list = []
-    for _ in range(args.max_objs):
-        obj_attr = generate_object_attribute(text_tokens, max_tokens, args.max_lines)
+    # 1. generate the image
+    img, att_list = multicaptcha_image_generator.generate_multi_captcha_image(
+        num_max_objs=num_max_objs, add_noise=True
+    )
 
-        if not att_intersect(att_list, obj_attr):
-            att_list.append(obj_attr)
-
-    # 2. insert the objects into the image
-    img = np.zeros((height, width, 3), dtype=np.uint8)
-    for att in att_list:
-        insert_object(img, att)
-    img = cv2.bitwise_not(img)
-
-    # 3. add noise
-    noise = np.random.randint(0, 255, img.shape, dtype=np.uint8)
-    img = cv2.addWeighted(img, 0.7, noise, 0.3, 0)
-
-    # 4. save the image
+    # 2. save the image
     if not os.path.exists(result_dir):
         os.makedirs(result_dir)
     img_path = os.path.join(result_dir, f"{image_idx}.jpg")  # jpg for compression
     cv2.imwrite(img_path, img)
 
-    # 5. save the labels as YOLO format
+    # 3. save the labels as YOLO format
     label_path = os.path.join(result_dir, f"{image_idx}.txt")
     with open(label_path, "w") as f:
         for att in att_list:
-            x = att["x"] + att["w"] / 2
-            y = att["y"] + att["h"] / 2
+            x = att["x"]
+            y = att["y"]
             w = att["w"]
             h = att["h"]
-            content = att["content"]
-            f.write(f"0 {x:.6f} {y:.6f} {w:.6f} {h:.6f} {''.join(content)}\n")
+            text_lines = att["text_lines"]
+            f.write(f"0 {x:.6f} {y:.6f} {w:.6f} {h:.6f} {''.join(text_lines)}\n")
 
 
 def main(args):
     spec = parse_spec(args.spec)
 
-    text_tokens = list(spec["text_tokens"])
-    max_tokens = int(spec["max_tokens"])
-    height = int(spec["imgh"])
-    width = int(spec["imgw"])
+    multicaptcha_image_generator = MultiCaptchaImageGenerator(
+        height=int(spec["imgh"]),
+        width=int(spec["imgw"]),
+        text_tokens=list(spec["text_tokens"]),
+        max_tokens=int(spec["max_tokens"]),
+        max_lines=args.max_lines,
+    )
 
     num_data = {
         "train": args.num_train,
@@ -262,10 +295,8 @@ def main(args):
         for i in tqdm(range(num)):
             gen_and_save_image(
                 image_idx=i,
-                text_tokens=text_tokens,
-                max_tokens=max_tokens,
-                height=height,
-                width=width,
+                num_max_objs=args.max_objs,
+                multicaptcha_image_generator=multicaptcha_image_generator,
                 result_dir=os.path.join(args.result_dir, partition),
             )
 
